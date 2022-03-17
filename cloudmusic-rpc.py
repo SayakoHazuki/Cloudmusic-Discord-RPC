e71a4b0a0e2078f92d4cfaed438b02dc07929c3f
Kai Kamf <74895715+Kai9073@users.noreply.github.com>
2022-03-17 23:04:51 +0800

    bug fixes

--- cloudmusic-rpc.py
+++ cloudmusic-rpc.py
@@ -1,3 +1,4 @@
+import os
 from pypresence import Presence
 import psutil
 import win32process
@@ -11,19 +12,18 @@ GetWindowText = ctypes.windll.user32.GetWindowTextW
 GetWindowTextLength = ctypes.windll.user32.GetWindowTextLengthW
 
 
-def get_process():
+def get_processes():
+    processes = []
     if "cloudmusic.exe" in (process.name() for process in psutil.process_iter()):
         for proc in psutil.process_iter():
             if proc.name() == "cloudmusic.exe":
-                return proc
-    else:
-        return None
+                processes.append(proc)
+    return processes
 
 
 def get_hwnds_for_pid(pid):
     def callback(hwnd, hwnds):
         _, found_pid = win32process.GetWindowThreadProcessId(hwnd)
-
         if found_pid == pid:
             hwnds.append(hwnd)
         return True
@@ -36,6 +36,8 @@ regex_pattern = re.compile("^(.*) - (.*)$")
 
 
 def get_track_by_hwnds(hwnds):
+    track_title = None
+    track_singer = None
     for hwnd in hwnds:
         length = GetWindowTextLength(hwnd)
         buff = ctypes.create_unicode_buffer(length + 1)
@@ -49,26 +51,35 @@ def get_track_by_hwnds(hwnds):
     return track_title, track_singer
 
 
-f = open('config.json')
+f = open(os.path.join(os.path.dirname(__file__), 'config.json'))
 config = json.load(f)
 RPC = Presence(config["appId"])
 
 temp_track_title = "Loading Title..."
 temp_track_singer = "Loading Vocalist..."
-RPC.connect()
-print('Connected to Discord!')
+is_connected = False
 while True:
-    process = get_process()
-    if (process and process.status() == 'running'):
-        track = get_track_by_hwnds(get_hwnds_for_pid(process.pid))
-        if(temp_track_title == track[0]):
-            pass
-        else:
-            temp_track_title = track[0]
-            temp_track_singer = track[1]
-            RPC.update(details=temp_track_title, state=temp_track_singer,
-                       large_image="cloudmusic", large_text="cloudmusic",
-                       start=int(time.time()))
-            print('Track updated - {} // {}'.format(temp_track_title,temp_track_singer))
-            time.sleep(15)
-        time.sleep(0.2)
+    processes = get_processes()
+    if (processes and not is_connected):
+        RPC.connect()
+        is_connected = True
+        print('Connected to Discord!')
+    if (not processes and is_connected):
+        RPC.close()
+        is_connected = False
+        print('Cloudmusic closed, closing connection to Discord.')
+    for process in processes:
+        if (process and process.status() == 'running'):
+            track = get_track_by_hwnds(get_hwnds_for_pid(process.pid))
+            if(temp_track_title == track[0]):
+                pass
+            elif(track[0]):
+                temp_track_title = track[0]
+                temp_track_singer = track[1]
+                RPC.update(details=temp_track_title, state=temp_track_singer,
+                           large_image=config["imgId"], large_text=config["imgTxt"],
+                           start=int(time.time()))
+                print(
+                    'Track updated - {} // {}'.format(temp_track_title, temp_track_singer))
+                time.sleep(15)
+    time.sleep(0.2)
